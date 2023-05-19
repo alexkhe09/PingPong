5551a74f1660e6c118cdd3656b55bcc0542287e6
Aleksei <79855232+alexkhe09@users.noreply.github.com>
2023-05-19 21:28:40 +0800

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -1,19 +1,36 @@
-class GameSprite(sprite.Sprite): #Создаю класс GameSprite(наследник класса Sprite)
-    def __init__(self, player_image, player_x, player_y, player_width, player_height, player_speed): #Создаю конструктор класса, передаю картинку спрайта, скорость, ширину, выстоу, x, y
-        super().__init__() #Вызываю конструктор суперкласса
-        self.image = transform.scale(image.load(player_image), (player_width, player_height))#Создаю и сохраняю картинку в переменной
-        self.speed = player_speed #Сохраняю скорость в переменную
-        self.rect = self.image.get_rect() #Создаю rect
-        self.rect.x = player_x #Сохраняю координату x в переменной
-        self.rect.y = player_y #Сохраняю координату y в переменной
+from pygame import *
 
-    def reset(self):#Создаю метод отрисовк спрайта
-        window.blit(self.image, (self.rect.x, self.rect.y))#Отрисовываю спрайт на окне
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_width, player_height, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (player_width, player_height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
 
-class Player(GameSprite):#Создаю класс Player
-    def update(self):#Создаю метод update, отвечающий за передвижение игрока
-        keys_pressed = key.get_pressed()#Получаю информацию о состоянии клавиш
-        if keys_pressed[K_LEFT] and self.rect.x >= 5:#Проверяю нажатие стрелки влево и положение корабля по координате х
-            self.rect.x -= self.speed#Вычитаю скорость из координаты х
-        if keys_pressed[K_RIGHT] and self.rect.x <= 660:#Проверяю нажатие стрелки вправо и положение корабля по координате х
-            self.rect.x += self.speed#Прибавляю скорость к координате х
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_LEFT] and self.rect.x >= 5:
+            self.rect.x -= self.speed
+        if keys_pressed[K_RIGHT] and self.rect.x <= 660:
+            self.rect.x += self.speed
+
+window = display.set_mode((700, 500))
+display.set_caption('Ping pong')
+background = transform.scale(image.load('background.png'), (700, 500))
+game = True
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    window.blit(background, (0, 0))
+
+    time.delay(50)
+    display.update()
