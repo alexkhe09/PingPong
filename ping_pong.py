218388612d375af1544a1962bf7c72a4260be2ec
Aleksei <79855232+alexkhe09@users.noreply.github.com>
2023-05-19 21:54:59 +0800

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -13,24 +13,38 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_LEFT] and self.rect.x >= 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_RIGHT] and self.rect.x <= 660:
-            self.rect.x += self.speed
+        if keys_pressed[K_UP] and self.rect.y >= 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y <= 420:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y >= 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y <= 420:
+            self.rect.y += self.speed
 
 window = display.set_mode((700, 500))
-display.set_caption('Ping pong')
+display.set_caption('Ping Pong')
 background = transform.scale(image.load('background.png'), (700, 500))
 game = True
+racket_r = Player('racket.png', 665, 220, 25, 75, 5)
+racket_l = Player('racket.png', 10, 220, 25, 75, 5)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     
+    racket_r.update_r()
+    racket_l.update_l()
+
     window.blit(background, (0, 0))
+    racket_r.reset()
+    racket_l.reset()
 
     time.delay(50)
     display.update()
